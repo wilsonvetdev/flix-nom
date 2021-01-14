81383a7abae3afbf22acd5b699b1bd46ec04ca04
wilsonng21 <wilsongch92@gmail.com>
2021-01-14 16:42:22 -0500

    movie search results rendering on DOM

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Grid, Header } from 'semantic-ui-react'
 import axios from 'axios'
 import SearchForm from './components/SearchForm'
+import Movies from './components/Movies'
 
 function App() {
 
   const API_KEY = process.env.REACT_APP_MOVIE_API_KEY
 
-  const [query, setQuery] = useState('')
   const [data, setData] = useState([])
 
-  useEffect(() => {
-    searchMovies(query)
-  }, [query])
-
-  const doSearch = input => setQuery(input)
-
-  const searchMovies = async(input) => {
+  const doSearch = async(input) => {
     if(input){
       try {
         let response = await axios.get(`http://www.omdbapi.com/?&apikey=${API_KEY}&type=movie&s=${input}`)
         setData(response.data)
-        console.log(input, response.data)
-        console.log(data)
       } catch(error) {
         console.error('Error fetching and parsing data', error)
       }
@@ -33,8 +25,9 @@ function App() {
     <Container>
       <Header as='h1' style={{padding: '1rem'}}>Flix Noms</Header>
       <SearchForm doSearch={doSearch}/>
+      <Movies movies={data}/>
     </Container>
-  );
+  )
 }
 
 export default App;
